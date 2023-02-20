bd6ade8529331ed0a1c4bbab7820423f18d8df09
Aditya Garg <gargaditya08@live.com>
2023-02-20 14:13:53 +0530

    Use 6.1 as new LTS

--- patch_driver.sh
+++ patch_driver.sh
@@ -6,7 +6,7 @@ BUILD_PATH=/tmp/build-kernel
 
 # Patches
 APPLE_SMC_DRIVER_GIT_URL=https://github.com/t2linux/linux-t2-patches.git
-APPLE_SMC_DRIVER_BRANCH_NAME=5.15
+APPLE_SMC_DRIVER_BRANCH_NAME=6.1
 APPLE_SMC_DRIVER_COMMIT_HASH=HEAD
 
 rm -rf "${BUILD_PATH}"
