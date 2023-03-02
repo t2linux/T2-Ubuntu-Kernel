0bf50ba7cabbab7d63b0cf43f9ae984625c30eda
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2023-03-02 17:37:54 +0530

    6.1.14-2

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y lsb-release
 
-KERNEL_VERSION=6.2.1
-PKGREL=1
+KERNEL_VERSION=6.1.14
+PKGREL=2
 CODENAME=$(lsb_release -c | cut -d ":" -f 2 | xargs)
 
 if [[ $USE_T2LINUX_REPO = true ]]
