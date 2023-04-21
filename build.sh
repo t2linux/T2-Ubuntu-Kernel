6a03b61646a05fe390d4cadec8a29fcdbc9f41ce
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2023-04-21 07:00:33 +0530

    Add python3 as a dependency

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ rm -rf "${KERNEL_PATH}"
 ### Dependencies
 apt-get install -y build-essential fakeroot libncurses-dev bison flex libssl-dev libelf-dev \
   openssl dkms libudev-dev libpci-dev libiberty-dev autoconf wget xz-utils git \
-  libcap-dev bc rsync cpio dh-modaliases debhelper kernel-wedge curl gawk dwarves zstd
+  libcap-dev bc rsync cpio dh-modaliases debhelper kernel-wedge curl gawk dwarves zstd python3
 
 ### get Kernel and Drivers
 if [[ $USE_T2LINUX_REPO = true ]]
