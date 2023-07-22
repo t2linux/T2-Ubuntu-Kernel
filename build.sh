cbb54a1b4c25cc0370e3a9240cc8a60fef6bd7f1
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2023-07-22 22:20:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -112,13 +112,6 @@ sed -i 's/CONFIG_MESSAGE_LOGLEVEL_DEFAULT=.*/CONFIG_MESSAGE_LOGLEVEL_DEFAULT=4/g
 
 # Copy the modified config
 cp "${WORKING_PATH}/templates/default-config" "${KERNEL_PATH}/.config"
-./scripts/config --undefine GDB_SCRIPTS
-./scripts/config --undefine DEBUG_INFO
-./scripts/config --undefine DEBUG_INFO_SPLIT
-./scripts/config --undefine DEBUG_INFO_REDUCED
-./scripts/config --undefine DEBUG_INFO_COMPRESSED
-./scripts/config --set-val  DEBUG_INFO_NONE       y
-./scripts/config --set-val  DEBUG_INFO_DWARF5     n
 make olddefconfig
 ./scripts/config --module CONFIG_HID_APPLE_IBRIDGE
 ./scripts/config --module CONFIG_HID_APPLE_TOUCHBAR
