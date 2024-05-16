ae862136405dc771b4f75946e741917e4b7e7bda
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2024-05-17 00:01:02 +0530

    Change variable names in patch_driver.sh

--- patch_driver.sh
+++ patch_driver.sh
@@ -5,19 +5,19 @@ set -eu -o pipefail
 BUILD_PATH=/tmp/build-kernel
 
 # Patches
-APPLE_SMC_DRIVER_GIT_URL=https://github.com/t2linux/linux-t2-patches.git
-APPLE_SMC_DRIVER_BRANCH_NAME=main
-APPLE_SMC_DRIVER_COMMIT_HASH=HEAD
+T2_PATCHES_GIT_URL=https://github.com/t2linux/linux-t2-patches.git
+T2_PATCHES_BRANCH_NAME=main
+T2_PATCHES_COMMIT_HASH=HEAD
 
 rm -rf "${BUILD_PATH}"
 mkdir -p "${BUILD_PATH}"
 cd "${BUILD_PATH}" || exit
 
 ### AppleSMC and BT aunali fixes
-git clone --single-branch --branch ${APPLE_SMC_DRIVER_BRANCH_NAME} ${APPLE_SMC_DRIVER_GIT_URL} \
+git clone --single-branch --branch ${T2_PATCHES_BRANCH_NAME} ${T2_PATCHES_GIT_URL} \
   "${BUILD_PATH}/linux-mbp-arch"
 cd "${BUILD_PATH}/linux-mbp-arch" || exit
-git checkout ${APPLE_SMC_DRIVER_COMMIT_HASH}
+git checkout ${T2_PATCHES_COMMIT_HASH}
 
 while IFS= read -r file; do
   echo "==> Adding ${file}"
