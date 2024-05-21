0b75a54df9058b5f718bbff66837804dd09d2d59
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2024-05-21 11:44:43 +0530

    6.9.1-2

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ apt-get update
 apt-get install -y lsb-release
 
 KERNEL_VERSION=6.9.1
-PKGREL=1
+PKGREL=2
 CODENAME=$(lsb_release -c | cut -d ":" -f 2 | xargs)
 
 #KERNEL_REPOSITORY=git://kernel.ubuntu.com/virgin/linux-stable.git
