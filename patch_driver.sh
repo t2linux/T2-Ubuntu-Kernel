832b915210bf2c0b69be0af5e79b70023c64e3bc
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2024-07-11 21:54:16 +0530

    Remove upstreamed patches

--- patch_driver.sh
+++ patch_driver.sh
@@ -6,8 +6,8 @@ BUILD_PATH=/tmp/build-kernel
 
 # Patches
 T2_PATCHES_GIT_URL=https://github.com/t2linux/linux-t2-patches.git
-T2_PATCHES_BRANCH_NAME=main
-T2_PATCHES_COMMIT_HASH=0ad2b3913f5484ba8e86b6965f5d88903464261d
+T2_PATCHES_BRANCH_NAME=6.9
+T2_PATCHES_COMMIT_HASH=HEAD
 
 rm -rf "${BUILD_PATH}"
 mkdir -p "${BUILD_PATH}"
@@ -18,8 +18,6 @@ git clone --single-branch --branch ${T2_PATCHES_BRANCH_NAME} ${T2_PATCHES_GIT_UR
   "${BUILD_PATH}/linux-mbp-arch"
 cd "${BUILD_PATH}/linux-mbp-arch" || exit
 git checkout ${T2_PATCHES_COMMIT_HASH}
-rm 2010*
-rm 1017*
 
 while IFS= read -r file; do
   echo "==> Adding ${file}"
