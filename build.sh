4b026f2bffcb0d515d4e887bd4c53eb445dae0c4
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2024-11-12 16:57:52 +0530

    Enable force unload

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ apt-get update
 apt-get install -y lsb-release
 
 KERNEL_VERSION=6.6.60
-PKGREL=1
+PKGREL=2
 DISTRO=$(lsb_release -i | cut -d ":" -f 2 | xargs)
 CODENAME=$(lsb_release -c | cut -d ":" -f 2 | xargs)
 
@@ -118,6 +118,7 @@ make olddefconfig
 ./scripts/config --module CONFIG_DRM_APPLETBDRM
 ./scripts/config --module CONFIG_BT_HCIBCM4377
 ./scripts/config --module CONFIG_APFS_FS
+./scripts/config --enable CONFIG_MODULE_FORCE_UNLOAD
 
 # Get rid of the dirty tag
 echo "" >"${KERNEL_PATH}"/.scmversion
