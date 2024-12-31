bdddf168a5f478b891ad261405836cfa02b7b0de
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2024-12-31 23:13:12 +0530

    6.6.68-3

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ apt-get update
 apt-get install -y lsb-release
 
 KERNEL_VERSION=6.6.68
-PKGREL=2
+PKGREL=3
 DISTRO=$(lsb_release -i | cut -d ":" -f 2 | xargs)
 CODENAME=$(lsb_release -c | cut -d ":" -f 2 | xargs)
 
